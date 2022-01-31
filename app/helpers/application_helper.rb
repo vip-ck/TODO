dce8ef3efd2adad34ab0c9870569ea67c41bf53c
vip <vip1313@gmail.com>
2022-01-31 23:20:31 +0200

    Lesson3 create

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def current_date
+    Date.current.to_s
+  end
+  def list_tag(arr)
+    tag.ul do
+      safe_join(arr.map { |line| raw tag.li(line) })
+    end
+  end
 end
