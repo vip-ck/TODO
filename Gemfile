aa6d566389031c5bfbb0addcf9c622313b93d907
vip <vip1313@gmail.com>
2022-02-01 02:38:26 +0200

    vipolnili lesson4

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem 'devise'
 gem 'devise-i18n'
 gem 'kaminari'
 gem 'pundit'
+gem 'rails-i18n'
 gem 'slim-rails'
 
 group :development, :test do
--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class AboutController < ApplicationController
-  def index; end
+  def index
+    session[:about_counter] ||= 0
+    session[:about_counter] += 1
+    locale = session[:locale]
+    I18n.locale = session[:locale] if I18n.available_locales.include?(locale).to_s
+  end
 end
